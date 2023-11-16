c23c193148c5d50fa554a5106f36ef1ec7a65071
Morgorita37249 <114601129+Morgorita37249@users.noreply.github.com>
2023-11-17 00:06:29 +0500

    Add files via upload

---
+++ BotMM.java
@@ -0,0 +1,48 @@
+package crv;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+
+public class BotMM extends TelegramLongPollingBot {
+    private final String name;
+    private String ResMessage="something wrong";
+    //private Map<Long, List<String>> UsersData = new HashMap<>(); //have all chat tokens and their state + needed prev data
+    public BotMM(String token, String name) {
+        super(token);
+        this.name = name;
+    }
+    /*public List<String> GetUserData(Long UserID){
+        return UsersData.get(UserID);
+    }*/
+    public static void main(String[] args) throws IOException, TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Secret secret = new Secret();
+        var token = secret.token; //need to change for file but for now will be empty and need to fill handy
+        var name = secret.token; //same as ^
+
+        telegramBotsApi.registerBot(new BotMM(token, name));
+    }
+    public void onUpdateReceived(Update update) {
+
+        SendMessage response = new SendMessage();
+        response.setText(MessageResponse.ProcessUpdate(update));
+        response.setChatId(update.getMessage().getChatId());
+        try {
+            execute(response);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            //throw new RuntimeException(e);
+
+        }
+
+    }
+
+    public String getBotUsername() {
+        return name;
+    }
---
+++ DataBase.java
@@ -0,0 +1,54 @@
+package crv;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// база чатов, хранит
+public class DataBase {
+
+    public static class TAGSCOLLECTION {
+        public static final String STATE="state";
+    }
+
+    // Хранилище состояний чатов - хешмап чат ид -> массив состояний, описанных тегами (хранилище значний имя-> значение)
+    public Map<Long, HashMap<String,String>> UsersData = new HashMap<>();
+    // Singleton
+    private static DataBase instance;
+    private DataBase(){}
+    public static DataBase GetInstance(){
+        if (instance == null) {
+            instance = new DataBase();
+        }
+        return instance;
+    }
+
+    // end Singleton
+
+    // прямое обращение к состоянию диалога (по тегу "State")
+    public String getDialogState(Long UserId){
+        return UsersData.get(UserId).get("state"); // TODO: переписать обращение по строке на обращение к константе класса TAGSCCOLLECTION
+    }
+
+    // создание нового чата, создание для него нового дескриптора с состоянием "новый"
+    public void newUser(Long UserId){
+        HashMap<String,String> newUserData = new HashMap<>();
+        newUserData.put(TAGSCOLLECTION.STATE,"new");
+        UsersData.put(UserId, newUserData);
+    }
+
+    // получение значения тега чата по имени. Переписать на обращение к константе класса tagscollection
+    public String getTag(Long UserID, String Tag){
+        return UsersData.get(UserID).get(Tag);
+    }
+
+    // изменение записи состояния чата по тегу. Тоже переписать
+    public void setTag(Long UserID, String strTag, String strNewValue){
+        var UserData = UsersData.get(UserID);
+        UserData.put(strTag,strNewValue);
+        UsersData.put(UserID,UserData);
+    }
+
+    public void uploadData(){}
+    public void downloadData(){}
+}
