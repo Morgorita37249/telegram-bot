c88f84b54f3ff5bb5647df58bc7d1261879853a6
Morgorita37249 <114601129+Morgorita37249@users.noreply.github.com>
2023-12-18 18:30:57 +0500

    Update Graph.java

--- src/main/java/crv/MessageBuilt/Graph.java
+++ src/main/java/crv/MessageBuilt/Graph.java
@@ -1,129 +1,135 @@
 package crv.MessageBuilt;
 
 import java.util.*;
+import java.util.Iterator;
 
-// Класс - хранилище карты ориентиров
+
 public class Graph {
     public class WayPoint {
         public String name;
         public String ID;
-        public List<WayPoint> edges=new ArrayList<WayPoint>();
+        public List<WayPoint> edges = new ArrayList<WayPoint>();
+
         public WayPoint(String name, String ID) {
-            this.name=name;
-            this.ID=ID;
+            this.name = name;
+            this.ID = ID;
             Graph.registerWayPoint(this);
         }
+
         public void connect(WayPoint otherWayPoint) {
             if (this == otherWayPoint) return;
-            if(!edges.contains(otherWayPoint)) edges.add(otherWayPoint);
+            if (!edges.contains(otherWayPoint)) edges.add(otherWayPoint);
         }
     }
 
-    private final Map<String,WayPoint> WayPoints = new HashMap<>();
+    private final Map<String, WayPoint> WayPoints = new HashMap<>();
 
     public WayPoint getWayPointByID(String id) {
         return WayPoints.get(id);
     }
 
     public void connectWayPoints(String id1, String id2) {
-        WayPoint wp1=WayPoints.get(id1);
-        WayPoint wp2=WayPoints.get(id2);
+        WayPoint wp1 = WayPoints.get(id1);
+        WayPoint wp2 = WayPoints.get(id2);
         wp1.connect(wp2);
         wp2.connect(wp1);
     }
 
-    // инициализация из файла на диске, написать позже
-    private void readMap() {
-        try {
-            throw new Exception("not implemented");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    // конструктор Singleton
     private static Graph instance = new Graph();
-    public static Graph getInstance()  {return instance;};
 
-    // Упрощающая процедура для регистрации новосозданных ориентиров
-    public static void registerWayPoint(WayPoint wp) {instance.WayPoints.put(wp.ID,wp);};
-
-    // для использования в интерфейсе администратора для заполнения графа посредством команд
-    public void newWaypoint(String name) {
-        new WayPoint(name,"id"+name);
+    public static Graph getInstance() {
+        return instance;
     }
 
 
-    public WayPoint getWayPointByName(String message) {
-        //Ищем в массиве ориентиров ориентир с похожим именем
-        // В сообщении может быть опечатка, пробелы, регистры.
-        // Здесь должна быть обработка, чтобы перед поиском оставить только нужный кусок.
-        // Либо искать, пробегая по массиву - если в сообщении встречается текст наименования, то возвращать его.
-        // НО тогда возможно, что найдётся более одного ориентира.
+    public static void registerWayPoint(WayPoint wp) {
+        instance.WayPoints.put(wp.ID, wp);
+    }
+
+
+    public void newWayPoint(String name) {
+        WayPoint wayPoint = new WayPoint(name, "id" + name);
+        registerWayPoint(wayPoint);
+    }
+
+
+    public WayPoint getWayPointByName(String name) {
+        if (instance != null) {
+            for (WayPoint wayPoint : instance.WayPoints.values()) {
+                if (wayPoint.name.equals(name)) {
+                    return wayPoint;
+                }
+            }
+        }
         return null;
     }
-    // Возвращаем строку вида "Точка 1 (вы здесь)" -> "Точка 2" -> .... -> "Точка N (вам сюда)"
-    //    from: ID исходной точки
-    //	  to:	ID целевой точки
 
-    public String getWay(WayPoint from, WayPoint to) {
-	//Используем алгоритм поиска в ширину
-	LinkedList<WayPoint> queue = new LinkedList<>();
-	HashMap<WayPoint, WayPoint> parentMap = new HashMap<>();
+    public ArrayList<WayPoint> getWay(WayPoint from, WayPoint to) throws IllegalArgumentException {
+        if (from == null || to == null) {
+            throw new IllegalArgumentException("Ошибка: Вершина не существует.");
+        }
 
-	queue.add(from);
-	parentMap.put(from, null);
+        LinkedList<WayPoint> queue = new LinkedList<>();
+        ArrayList<WayPoint> Way = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        queue.add(from);
+        visited.add(from.ID);
 
-	while (!queue.isEmpty()) {
-		WayPoint current = queue.poll();
+        while (!queue.isEmpty()) {
+            WayPoint current = queue.poll();
+            Way.add(current);
 
-		if (current == to) {
-			return reconstructPath(parentMap, from, to);
-		}
+            for (WayPoint neighbor : current.edges) {
+                if (!visited.contains(neighbor.ID)) {
+                    queue.add(neighbor);
+                    visited.add(neighbor.ID);
 
-		for (WayPoint neighbor : current.edges) {
-			if (!parentMap.containsKey(neighbor)) {
-				queue.add(neighbor);
-				parentMap.put(neighbor, current);
-			}
-		}
-	}
-
-	return "Путь не найден";
+                    if (neighbor.ID.equals(to.ID)) {
+                        return Way;
+                    }
+                }
+            }
+        }
+        throw new IllegalArgumentException("No way between " + from.ID + " и " + to.ID);
     }
 
-    public String reconstructPath(Map<WayPoint, WayPoint> parentMap, WayPoint start, WayPoint end) {
-	LinkedList<WayPoint> path = new LinkedList<>();
-	WayPoint current = end;
-
-	while (current != null) {
-		path.addFirst(current);
-		current = parentMap.get(current);
-	}
-
-	StringBuilder result = new StringBuilder("Путь: ");
-	for (WayPoint point : path) {
-		result.append(point.name).append(" -> ");
-	}
-	result.delete(result.length() - 4, result.length());
-
-	return result.toString();
-    }
-    public List<String> get_Names(){
+    public List<String> get_Names(List<WayPoint> wayPoints) {
         List<String> names = new ArrayList<>();
-        return  names;
+        for (WayPoint wayPoint : wayPoints) {
+            names.add(wayPoint.name);
+        }
+        return names;
     }
 
     private Graph() {
-        //readMap();
 
-        //Дубовое заполнение графа кодом
-        new WayPoint("501","id501");
-        new WayPoint("502","id502");
-        new WayPoint("503","id503");
-        connectWayPoints("id501","id502");
-        connectWayPoints("id502","id503");
+        Map<String, String> WayPoints = JSONData.readJsonData();
+
+        for (Map.Entry<String, String> currentEntry : WayPoints.entrySet()) {
+            String currentKey = currentEntry.getKey();
+            String currentValue = currentEntry.getValue();
+            new WayPoint(currentValue, currentKey);
+
+
+            for (Map.Entry<String, String> entry : WayPoints.entrySet()) {
+                String nextKey = entry.getKey();
+                String nextValue = entry.getValue();
+                new WayPoint(nextValue, nextKey);
+
+                if (currentKey.contains("id5") && nextKey.contains("id5") ||
+                        currentKey.contains("id6") && nextKey.contains("id6") ||
+                        currentKey.equals("id5L1") && nextKey.equals("id6L1") ||
+                        currentKey.equals("id5L2") && nextKey.equals("id6L2") ||
+                        currentKey.equals("id5L3") && nextKey.equals("id6L3") ||
+                        currentKey.equals("id5L") && nextKey.equals("id6L")) {
+
+                    connectWayPoints(currentKey, nextKey);
+                }
+            }
+        }
     }
 }
+
 
 
