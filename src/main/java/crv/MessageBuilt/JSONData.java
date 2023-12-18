4174789673407e1640fa3410552beb2781cc4d35
Morgorita37249 <114601129+Morgorita37249@users.noreply.github.com>
2023-12-18 18:34:19 +0500

    Add files via upload

---
+++ src/main/java/crv/MessageBuilt/JSONData.java
@@ -0,0 +1,57 @@
+package crv.MessageBuilt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Iterator;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+public class JSONData{
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String filePath = "src/main/resources/classes.json";
+
+    private static void addNewData(String newKey, String newValue) {
+        try {
+            JsonNode existingJson = objectMapper.readTree(new File(filePath));
+            if (!existingJson.has(newKey)) {
+                ((com.fasterxml.jackson.databind.node.ObjectNode) existingJson).put(newKey, newValue);
+                objectMapper.writeValue(new File(filePath), existingJson);
+                System.out.println("Пара " + newKey + ": " + newValue + " успешно добавлена.");
+            } else {
+                System.out.println( newValue + "место уже существует");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Map<String, String> readJsonData() {
+        Map<String, String> dataMap = new HashMap<>();
+
+        try {
+            JsonNode jsonData = objectMapper.readTree(new File(filePath));
+            Iterator<Map.Entry<String, JsonNode>> fields = jsonData.fields();
+
+            while (fields.hasNext()) {
+                Map.Entry<String, JsonNode> field = fields.next();
+                String key = field.getKey();
+                String value = field.getValue().asText();
+
+                dataMap.put(key, value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return dataMap;
+    }
+
+    private static String getValueByKey(JsonNode jsonNode, String key) {
+        if (jsonNode.has(key)) {
+            return jsonNode.get(key).asText();
+        } else {
+            throw new IllegalArgumentException("Key '" + key + "' not found in JSON");
+        }
+    }
+}
